cc61aa315de95df0daac515eda68a899561b0a08
TheVulgarBulgar <41118324+TheVulgarBulgar@users.noreply.github.com>
2018-09-10 14:24:21 +0200

    Relaunch with working front-end

---
+++ Storefront/src/main/java/main/ProductDao.java
@@ -0,0 +1,17 @@
+package main;
+
+import java.util.Collection;
+
+public interface ProductDao {
+
+	Collection<Product> getAllProducts();
+
+	Product getProductById(int id);
+
+	void removeProductById(int id);
+
+	void updateProduct(Product product);
+
+	void insertStudent(Product product);
+
+}
