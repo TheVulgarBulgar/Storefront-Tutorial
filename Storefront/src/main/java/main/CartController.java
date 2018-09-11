588f15aa87259e606a087420f08446831d57c3f2
TheVulgarBulgar <41118324+TheVulgarBulgar@users.noreply.github.com>
2018-09-11 09:56:03 +0200

    Adding styling

--- Storefront/src/main/java/main/CartController.java
+++ Storefront/src/main/java/main/CartController.java
@@ -6,25 +6,25 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/products")
 public class CartController {
 
 	@Autowired
 	private ProductService productService;
 	
 	@ModelAttribute("products")
-	@RequestMapping(method = RequestMethod.GET)
+	@GetMapping("products")
 	public Iterable<Product> getAllProducts(){
 		return this.productService.getAllProducts();
 	}
 	
-	@RequestMapping(value="/{id}", method = RequestMethod.GET)
+	@RequestMapping(value="/products/{id}", method = RequestMethod.GET)
 	public Optional<Product> getProductById(@PathVariable("id") int id) {
 		return this.productService.getProductById(id);
 	}
@@ -38,9 +38,15 @@ public class CartController {
 	public void updateProduct(@RequestBody Product product){
 		this.productService.updateProduct(product);
 	}
-	
-	@RequestMapping(method = RequestMethod.POST)
-	public void insertStudent(@RequestBody Product product){
+
+	@PostMapping("products")
+	public void insertStudent(@ModelAttribute Product product){
 		this.productService.insertStudent(product);
+	}
+	
+	@ModelAttribute("product")
+	@GetMapping("add")
+	public Product AddProduct() {
+		return new Product();
 	}
 }
--- Storefront/src/main/java/main/ProductRepository.java
+++ Storefront/src/main/java/main/ProductRepository.java
@@ -1,6 +1,5 @@
 package main;
 
-import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
